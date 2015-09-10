28048935b7f30593b12863138ef50d3ea5ae554e
Udhayakumar Krishnan <udhayakumar.krishnan@scientificgames.com>
2015-09-10 15:27:51 +0530

    Added HTML

--- registration-bookmarklet.js
+++ registration-bookmarklet.js
@@ -1,7 +1,7 @@
 javascript: (function() {
     var head = document.getElementsByTagName('head')[0];
     var script = document.createElement('script');
-    script.src = 'https://cdn.rawgit.com/udhayakumaran/bookmarklet/master/registration.js?cache='+Math.random();
+    script.src = 'https://cdn.rawgit.com/udhayakumaran/bookmarklet/3ad45bfd5d876a369e87adcd84d3bcbfb388ddab/registration.js?cache='+Math.random();
     script.async = true;
     head.appendChild(script);
 })();
